b07f452a5c5d10187b7daf17b0b143fc9f1bab96
Dede Firmansah <dedefirmansah0508@gmail.com>
2024-06-23 17:31:15 +0700

    add:check authorize jwt

--- src/main/java/com/example/api/portfolio/config/SecurityConfig.java
+++ src/main/java/com/example/api/portfolio/config/SecurityConfig.java
@@ -1,24 +1,47 @@
 package com.example.api.portfolio.config;
 
+import com.example.api.portfolio.security.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/authenticate").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/public/**").permitAll() // Endpoint publik yang bisa diakses tanpa otentikasi
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll() // Endpoint untuk mendaftar pengguna baru
+                        .anyRequest().authenticated() // Semua permintaan lainnya memerlukan otentikasi
                 )
-                .csrf(csrf -> csrf.disable());
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Tidak menyimpan sesi pengguna
+                )
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/**").permitAll()
+//                )
+//                .csrf(csrf -> csrf.disable());
+//        return http.build();
+//    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
