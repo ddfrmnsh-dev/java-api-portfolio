188245ba380c1f48ed566bf220ee9826b800678e
Dede Firmansah <dedefirmansah0508@gmail.com>
2024-06-24 15:16:13 +0700

    add new: model project & Auth Controller

---
+++ src/main/java/com/example/api/portfolio/controller/AuthController.java
@@ -0,0 +1,4 @@
+package com.example.api.portfolio.controller;
+
+public class AuthController {
+}
--- src/main/java/com/example/api/portfolio/model/Project.java
+++ src/main/java/com/example/api/portfolio/model/Project.java
@@ -4,29 +4,27 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = "projects")
+public class Project {
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
     private String name;
 
-    @Column(name = "email")
-    private String email;
+    @Column(name = "slug")
+    private String slug;
 
-    @Column(name = "password")
-    private String password;
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
 
-    @Column(name= "is_active")
-    private boolean isActive;
+    @Column(name = "link_web")
+    private String linkWebsite;
 
     @Column(name = "created_at", updatable = false)
     @CreationTimestamp
@@ -36,24 +34,30 @@ public class User {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
-    public User() {}
-    public User(String name, String email, String password, boolean isActive){
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Project(){
+
+    }
+    public Project(String name, String slug, String description, String linkWebsite){
         this.name = name;
-        this.email = email;
-        this.password = password;
-        this.isActive = isActive;
+        this.slug = slug;
+        this.description = description;
+        this.linkWebsite = linkWebsite;
     }
 
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
-    public boolean getIsActive() {return isActive;}
-    public void setIsActive(boolean isActive) {this.isActive = isActive;}
+    public String getSlug() {return slug;}
+    public void setSlug(String slug) {this.slug = slug;}
+    public String getDescription() {return description;}
+    public void setDescription(String description) {this.description = description;}
+    public String getLinkWebsite() {return linkWebsite;}
+    public void setLinkWebsite(String linkWebsite) {this.linkWebsite = linkWebsite;}
     public LocalDateTime getCreatedAt() {return createdAt;}
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
